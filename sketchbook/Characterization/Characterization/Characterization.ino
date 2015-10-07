de82e14fcc0c47a10312665280191846e4119f50
lbeaufore <4beau4@gmail.com>
2015-10-07 19:57:04 -0400

    New code format, issues with turn distances
    Seems like it could be functional. Need to test forward driving in order
    to debug.

--- sketchbook/Characterization/Characterization/Characterization.ino
+++ sketchbook/Characterization/Characterization/Characterization.ino
@@ -22,57 +22,126 @@ static int pulseRatio = 1200; // Number of encoder counts per rotation
 static double wheelDiameter = 7.75; // Diameter of the wheels in inches
 static double wheelBase = 36; // Distance between wheels in inches
 
-//Initialize PID parameters:
-// robot(countratio,wheel diameter,wheel-to-wheel distance,Kp,Ki,Kd,Kdiff,Kidiff)
-PID robot(pulseRatio, wheelDiameter, wheelBase, .01, .01, .01, .01, .5); // Wheel diameter used to be set to 10, unsure 
+// Global Variables
+long long countR, countL; // Encoder count
+bool stateR, stateL, lastStateR, lastStateL, left, right;
+double currentAngle, targetAngle; // The current angle of the robot, relative to start. Want to use gyro for this eventually
+unsigned long lastTime; // Last recorded time in milliseconds
 
+// Function prototypes
+void turnInPlace(int dir, double angle, double setspeed);
+void turnRightPivot(double setspeed);
+void turnLeftPivot(double setspeed);
+void stopMotors();
+void zeroEncoders();
+double encounterToDistance(int count);
 
-// Function that uses beacon data to determine the direction the robot is facing
-// Turns the robot and sends it forward 150 (cm?) at speed 15
-void beaconCallback( const std_msgs::Float64& angle)
+void setup()
 {
-  //readyForData.data=false;
-  //beaconRequest.publish(&readyForData);
-  //3 feet
-  double distance = 150;
-  // If the needed angle is more than 30 degrees off, turn and check again
-  if (angle.data >= 30 || angle.data <= -30)
-  {
-    //turnInPlace(0,45,150);
-  }
-  // If the angle is >0, turn right til straight
-  else if (angle.data > 0) {
-    //Turn to the angle and go
-    turnInPlace(0, angle.data, 250);
-    robot.movexinches(distance, 15);
-  }
-  // If the angle is <0, turn left til straight
-  else {
-    turnInPlace(1, angle.data, 250);
-    robot.movexinches(distance, 15);
-  }
-
-  readyForData.data = true;
-  beaconRequest.publish(&readyForData);
-
+  //  nh.initNode();
+  //  nh.subscribe(sub);
+  //  nh.advertise(beaconRequest);
+  md1.init();
+  Serial.begin(9600);
+  //Initialize PID parameters:
+  PID robot(pulseRatio, wheelDiameter, wheelBase, .01, .01, .01, .01, .5); // Wheel diameter used to be set to 10, unsure of why
+  zeroEncoders();
+  currentAngle = 0;
+  left = 0;
+  right = 0;
+  lastTime = millis();
+  targetAngle = 45; // Dangerous to have this here, but used in testing
 }
 
-//Command 0=straight   data[1]=distance  data[2]=speed
-//Command 1=turnRight  data[1]=angle     data[2]=speed
-//Command 2=turnLeft   data[1]=angle     data[2]=speed
-void commandCallback(const std_msgs::Float64MultiArray& command)
+// This code will run continuously while the arduino is powered
+void loop()
 {
-  if (command.data[0] == 0)
-  {
-    robot.movexinches(command.data[1], command.data[2]);
-  } else if (command.data[0] == 1) {
-    turnRight(command.data[1], command.data[2]);
-  } else if (command.data[0] == 2) {
-    turnLeft(command.data[1], command.data[2]);
+  /*if(millis() - lastTime > 10000){
+    targetAngle = -targetAngle;
+    zeroEncoders();
+    lastTime = millis();
+  }*/
+
+  if(abs(targetAngle - currentAngle) > 5) { // With 5 degrees of precision
+    if(targetAngle > currentAngle) {
+      left = 0;
+      right = 1;
+    } else {
+      left = 1;
+      right = 0;
+    }
+  } else {
+    left = 0;
+    right = 0;
   }
+  
+  if (left) {
+    turnLeftPivot(120);
+  } else if (right) {
+    turnRightPivot(120);
+  } else {
+    stopMotors();
+  }
+  
+  // Update encoders
+  stateL = (analogRead(A0) > 500); 
+  if (lastStateL != stateL && stateL) { // On change in pulse, increment
+    countL++;
+  }
+  lastStateL = stateL;
+  
+  stateR = (analogRead(A1) > 500); 
+  if (lastStateR != stateR && stateL) { // On change in pulse, increment
+    countR++;
+  }
+  lastStateR = stateR;
+  currentAngle = 360.0 * (encoderToDistance(countL)-encoderToDistance(countR))/(3.14159*wheelDiameter);
 }
 
+void stopMotors() {
+  md1.setM1Speed(0);
+  md1.setM2Speed(0);
+}
 
+// Function that makes the robot turn right by changing the left wheel's speed
+void turnRightPivot(double setspeed)
+{
+  // Set left wheels speed
+  md1.setM1Speed(setspeed);
+  md1.setM2Speed(0);
+}
+
+// Function that makes the robot turn right by changing the left wheel's speed
+void turnLeftPivot(double setspeed)
+{
+  // Set left wheels speed
+  md1.setM1Speed(0);
+  md1.setM2Speed(setspeed);
+}
+
+void zeroEncoders() {
+  countL = 0;
+  countR = 0;
+  lastStateR = 0;
+  lastStateL = 0;
+  stateR = 0;
+  stateL = 0;
+}
+
+double encoderToDistance(int counts) {
+  return (counts/pulseRatio*1.0)*3.14159*wheelDiameter;
+}
+
+//ros::Subscriber<std_msgs::Float64> sub("beacon_data", &beaconCallback);
+//ros::Subscriber<std_msgs::Float64MultiArray> comndSub("command", &commandCallback);
+
+
+
+
+// OLD, UNUSED CODE
+
+/*
+ 
 // Function that turns the robot without moving
 // dir={0:left or 1:right}
 // angle={desired turn angle}
@@ -149,18 +218,6 @@ void turnRight(double angle, double setspeed)
     }
     laststate = state;
     i++;
-
-    // IMPORTANT NOTE: Updating serial monitor takes so long that the encoder will miss updates
-    
-    /*Serial.print("Encoder Reads: ");
-    Serial.print(analogRead(A0));
-    Serial.print("\t");
-    Serial.print("Counter Reads: ");
-    Serial.print(counter);
-    Serial.print("/");
-    Serial.print(countmax);
-    Serial.print("\n");
-    */
   }
 
   // Returns left wheel speed to zero
@@ -204,49 +261,51 @@ void turnLeft(double angle, double setspeed)
   md1.setM2Speed(0);
 }
 
-ros::Subscriber<std_msgs::Float64> sub("beacon_data", &beaconCallback);
-ros::Subscriber<std_msgs::Float64MultiArray> comndSub("command", &commandCallback);
-
-
-
-void setup()
+// Function that uses beacon data to determine the direction the robot is facing
+// Turns the robot and sends it forward 150 (cm?) at speed 15
+void beaconCallback( const std_msgs::Float64& angle)
 {
-  //  nh.initNode();
-  //  nh.subscribe(sub);
-  //  nh.advertise(beaconRequest);
-  md1.init();
-  Serial.begin(9600);
+  //readyForData.data=false;
+  //beaconRequest.publish(&readyForData);
+  //3 feet
+  double distance = 150;
+  // If the needed angle is more than 30 degrees off, turn and check again
+  if (angle.data >= 30 || angle.data <= -30)
+  {
+    //turnInPlace(0,45,150);
+  }
+  // If the angle is >0, turn right til straight
+  else if (angle.data > 0) {
+    //Turn to the angle and go
+    turnInPlace(0, angle.data, 250);
+    robot.movexinches(distance, 15);
+  }
+  // If the angle is <0, turn left til straight
+  else {
+    turnInPlace(1, angle.data, 250);
+    robot.movexinches(distance, 15);
+  }
 
-}
-int count = 0;
-//int counter=0;
-//int state=0;
-//int laststate=0;
-
-void loop()
-{
-  // Just go with no encoder feedback
-  //  md1.setM1Speed(400);
-  //  md1.setM2Speed(400);
-  //  delay(10000);
-  //  md1.setM1Speed(0);
-  //  md1.setM2Speed(0);
-  //  delay(5000);
-  //  md1.setM1Speed(400);
-  //  md1.setM2Speed(-400);
-  //  delay(1000);
-  //  md1.setM1Speed(-400);
-  //  md1.setM2Speed(400);
-  //  delay(1000);
-  // Move and turn with encoder feedback
-  // delay(1000);
-  // robot.movexinches(90,15);
-  delay(2000);
-  //turnRight(60, -150);
-  turnInPlace(0,360,150);
-  delay(2000);
-  //turnLeft(60, -150);
-  turnInPlace(1,360,200);
+  readyForData.data = true;
+  beaconRequest.publish(&readyForData);
 
 }
 
+
+//Command 0=straight   data[1]=distance  data[2]=speed
+//Command 1=turnRight  data[1]=angle     data[2]=speed
+//Command 2=turnLeft   data[1]=angle     data[2]=speed
+void commandCallback(const std_msgs::Float64MultiArray& command)
+{
+  if (command.data[0] == 0)
+  {
+    robot.movexinches(command.data[1], command.data[2]);
+  } else if (command.data[0] == 1) {
+    turnRight(command.data[1], command.data[2]);
+  } else if (command.data[0] == 2) {
+    turnLeft(command.data[1], command.data[2]);
+  }
+}
+
+
+*/
