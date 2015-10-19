3c620464d0ed76bb10a7fdb78e7aea62ab454e69
MachVisi <OSUPhysicsCapstoneMotion@gmail.com>
2015-10-19 19:26:36 -0400

    Troubleshoot BT-Android in SerialCom

--- sketchbook/Characterization/SerialCom/SerialCom/SerialCom.ino
+++ sketchbook/Characterization/SerialCom/SerialCom/SerialCom.ino
@@ -18,24 +18,25 @@ SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 void setup()
 {
+
   Serial.begin(9600);  // Begin the serial monitor at 9600bps
 
   bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
-  bluetooth.print("$");  // Print three times individually
-  bluetooth.print("$");
-  bluetooth.print("$");  // Enter command mode
-  delay(100);  // Short delay, wait for the Mate to send back CMD
-  bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
-  // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
-  bluetooth.begin(9600);  // Start bluetooth serial at 9600
+//  bluetooth.print("$");  // Print three times individually
+//  bluetooth.print("$");
+//  bluetooth.print("$");  // Enter command mode
+//  delay(100);  // Short delay, wait for the Mate to send back CMD
+//  bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
+//  // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
+//  bluetooth.begin(9600);  // Start bluetooth serial at 9600
 }
 
 void loop()
 {
   if(bluetooth.available())  // If the bluetooth sent any characters
-  {
+  { 
     // Send any characters the bluetooth prints to the serial monitor
-    Serial.print((char)bluetooth.read());  
+    Serial.print((int)bluetooth.read());
   }
   if(Serial.available())  // If stuff was typed in the serial monitor
   {
