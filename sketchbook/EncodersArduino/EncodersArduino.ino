c98082f2d1c7a934abec85fab5b94fe4e31cf630
MachVisi <OSUPhysicsCapstoneMotion@gmail.com>
2015-10-30 13:19:00 -0400

    Split left/right motors/encoders into separate topics

---
+++ sketchbook/EncodersArduino/EncodersArduino.ino
@@ -0,0 +1,43 @@
+/*
+ *  Arduino code to read the encoders and give that information to ROS
+ */
+
+
+#include <ros.h>
+#include <Encoder.h>
+#include <std_msgs/Int8.h>
+
+// Set up ROS publishing
+ros::NodeHandle  nh;
+std_msgs::Int8 leftencoder_msg;
+ros::Publisher pub_encoders("Encoders", &leftencoder_msg);
+
+Encoder leftEnc(A0, A2); // Create left encoder object
+Encoder rightEnc(A1, A3); // Create right encoder object
+
+int newLeftPos = leftEnc.read();
+int newRightPos = rightEnc.read();
+int oldLeftPos = -999;
+int oldRightPos = -999;
+
+void setup()
+{  
+  nh.initNode();
+  nh.advertise(pub_encoders);
+}
+
+
+
+void loop()
+{
+  // If the encoder positions have changed, update their values with ROS
+  if (newLeftPos != oldLeftPos) {
+    oldLeftPos = newLeftPos;
+    leftencoder_msg.data = newLeftPos;
+  }
+  if (newRightPos != oldRightPos) {
+    oldRightPos = newRightPos;
+//    rightencoders_msg.data = newRightPos;
+  }
+  nh.spinOnce();
+}
