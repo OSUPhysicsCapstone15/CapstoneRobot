6ea6d2c10af6f8961cdccaf8c43881935f824d22
rvansoelen <vansoelen.1@osu.edu>
2015-11-23 17:03:57 -0500

    Added some stuff to vision

--- vision/beacontest.cpp
+++ vision/beacontest.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-//#include "opencv2/opencv.hpp"
+#include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <vector>
@@ -21,6 +21,49 @@ Point computeCentroid(const cv::Mat &mask) {
     Moments m = moments(mask, true);
     Point center(m.m10/m.m00, m.m01/m.m00);
     return center;
+}
+
+
+//get robot and camera angles
+//img -> the current camera image
+//objectRows -> rows from the left that the object is in
+//objectCols -> columns from the top that the object is
+void tilt_turn_degrees(Mat img, int object_rows, int object_cols)
+{
+    double camera_height = 1.2;     // height of camera from ground in meters
+    int camera_diagonal_angle = 69; // diagonal angle of view for camera in degrees
+                                    // logitech c525 fov is 69 degrees, Samsung Galaxy S5 is 90 degrees
+    
+    int rows = img.rows; // height of camera image in pixels
+    int cols = img.cols; // width of camera image in pixels
+    //cout << "Rows: " << rows << "\n" << "Cols: " << cols << endl;
+    
+    //logitech c525 fov is 69 degrees, Samsung Galaxy S5 is 90 degrees
+    double camera_diagonal = 69; // the angle of the cameras diagonal in degrees
+    double pixel_diagonal = sqrt(rows * rows + cols * cols); // (pythagorean) diagonal length of image in pixels
+    double degrees_per_pixel = camera_diagonal / pixel_diagonal; // ratio of real world degrees to pixels in the image
+    
+    int center_rows = rows / 2; // the center height is half of the total height
+    int center_cols = cols / 2; // the center width is half of the total width
+    //cout << "Center Rows: " << center_rows << "\n" << "Center Cols: " << center_cols << endl;
+    
+    int diff_rows = center_rows - object_rows; // difference between center and object rows
+    int diff_cols = center_cols - object_cols; // difference between center and object cols
+    //cout << "Diff Rows: " << diff_rows << "\n" << "Diff Cols: " << diff_cols << endl;
+    
+    double turn_robot_x_degrees = diff_cols * degrees_per_pixel; // positive -> turn left, negative -> turn right
+    double tilt_camera_x_degrees = diff_rows * degrees_per_pixel; // positive -> tilt up, negative -> tilt down
+    cout << "Turn robot " << turn_robot_x_degrees << " degrees.\n" << "Tilt camera " << tilt_camera_x_degrees << " degrees." << endl;
+    
+    double tilted_degrees = 90 - tilt_camera_x_degrees; // assuming camera is parallel to ground (90 degrees)
+
+    double tilted_radians = tilted_degrees * 3.1415962 / 180.0; // c++ tan() function uses radians
+
+    double height = camera_height; // height of camera from the ground in meters
+
+    double distance = height * tan(tilted_radians); // triangle formula for finding distance
+
+    cout << "Distance is " << distance << " meters" << endl;
 }
 
 
@@ -61,7 +104,7 @@ Point computeCentroid(const cv::Mat &mask) {
 
   string text("Object not found");
 
-    VideoCapture cap(1); //capture the video from web cam
+    VideoCapture cap(0); //capture the video from web cam
     if ( !cap.isOpened() )  // if not success, exit program
     {
          cout << "Cannot open the web cam" << endl;
@@ -88,6 +131,7 @@ cap>>imgOriginalON;
 cout<<"Taking OFF in "<<timer<<" s"<<endl;
 
 start = std::clock();
+duration=0;
 while(duration<timer){
     cap>>imgOriginalOFF;
     duration = (clock() - start ) / (double) CLOCKS_PER_SEC;
@@ -124,8 +168,9 @@ cap>>imgOriginalOFF;
   erode(imgThresholdedOFF, imgThresholdedOFF, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
 
   Mat imgTHRESH=imgThresholdedON-imgThresholdedOFF, out;
-  SimpleBlobDetector blobDetect = SimpleBlobDetector(params);
-  blobDetect.detect( imgTHRESH, keypoints );
+  //SimpleBlobDetector blobDetect = SimpleBlobDetector(params);
+  Ptr<SimpleBlobDetector> blobDetect = SimpleBlobDetector::create(params);
+  blobDetect->detect( imgTHRESH, keypoints );
 
   drawKeypoints(imgTHRESH, keypoints, out, CV_RGB(0,0,0), DrawMatchesFlags::DEFAULT);
   //Circle blobs
@@ -137,13 +182,14 @@ cap>>imgOriginalOFF;
    if(keypoints.size() == 2){
     text = "Object Found";
     cout<<endl<<endl<<"Object Found"<<endl;
+    Mat diff=imgThresholdedON-imgThresholdedOFF;
     int xCord=((keypoints[0].pt.x)+(keypoints[1].pt.y))/2;
-    int yCord=abs((keypoints[1].pt.y)-(keypoints[0].pt.y))/2
-    tilt_turn_degrees(img, yCord, xCord);
+    int yCord=abs((keypoints[1].pt.y)-(keypoints[0].pt.y))/2;
+    tilt_turn_degrees(diff, yCord, xCord);
   }
   else{
     text = "Error";
-    cout<<"No Object Found"<<endl;
+    cout<<endl<<endl<<"No Object Found"<<endl;
   }
 
   putText(out, text, Point(100,200), FONT_HERSHEY_PLAIN, 20, Scalar(0, 0, 255), 20);
@@ -162,7 +208,7 @@ cap>>imgOriginalOFF;
             //break;
  //      }
  //   }
-	waitKey(-1);
+	//waitKey(-1);
 }
    return 0;
 
