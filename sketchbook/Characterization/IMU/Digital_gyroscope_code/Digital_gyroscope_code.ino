9af62be0b083aecb978f902e62d2f14109aff747
MachVisi <OSUPhysicsCapstoneMotion@gmail.com>
2016-01-19 18:07:12 -0500

    working rosserial

--- sketchbook/Characterization/IMU/Digital_gyroscope_code/Digital_gyroscope_code.ino
+++ sketchbook/Characterization/IMU/Digital_gyroscope_code/Digital_gyroscope_code.ino
@@ -2,18 +2,20 @@
 #include <L3G.h>
 L3G gyro;
 
-void dc_offset() {
+void setup() {
+  
+  
   Serial.begin(9600);
 Wire.begin();
 
-while (!gyro.init());
 gyro.enableDefault();
 
 int sampleNum=500;
 int dc_offset=0;
 
 //Calculate initial DC offset and noise level of gyro
-for(int n=0;n<sampleNum;n++){
+for(int n=0;n<sampleNum;n++)
+{
 gyro.read();
 dc_offset+=(int)gyro.g.z;
 }
@@ -25,3 +27,5 @@ Serial.print(dc_offset);
 Serial.println();
 
 }
+
+void loop(){}
