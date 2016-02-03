c738313f5cb73c10edac6aa50f2eab38c62c9ffe
rvansoelen <vansoelen.1@osu.edu>
2016-02-03 18:41:00 -0500

    Added ROS message structs for beacon

--- vision/master_run/beacon.h
+++ vision/master_run/beacon.h
@@ -3,4 +3,18 @@
 
 int beacon_main(int cam);
 
+struct beacon_req {
+	double angle_min;
+	double angle_max;
+};
+
+struct beacon_rsp {
+	double dist; 
+	double theta; //angle from beacon to robot
+	double phi; //angle from robot to beacon
+	bool only_bottom;
+	bool not_found;
+	bool bad_theta;
+};
+
 #endif
